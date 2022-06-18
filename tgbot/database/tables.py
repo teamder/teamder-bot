5310095406f27ba196e6bf0d3b930f42db0d38d0
Ninnjah <yand.man4@gmail.com>
2022-06-19 02:33:03 +0300

    Add first table to database

---
+++ tgbot/database/tables.py
@@ -0,0 +1,18 @@
+"""Contains database tables info"""
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, BigInteger, \
+    Column, DateTime, String
+
+metadata = MetaData()
+
+users = Table(
+    "users", metadata,
+    Column("user_id", BigInteger(), primary_key=True),
+    Column("firstname", String(), nullable=False),
+    Column("lastname", String(), nullable=True),
+    Column("username", String(), nullable=True),
+    Column("lang", String(), default="ru"),
+    Column("created_on", DateTime(), default=datetime.now),
+    Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now)
+)
