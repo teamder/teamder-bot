ceb9ce2a6abfe34f4d9af6c07a84d22980239694
Ninnjah <yand.man4@gmail.com>
2022-06-19 06:20:48 +0300

    Add admin add confirm callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -2,4 +2,5 @@ from aiogram.utils.callback_data import CallbackData
 
 # Callback data just for example
 example_cb = CallbackData("example", "some_data")
+admin_add_conf_cb = CallbackData("admin_add_conf", "user_id")
 admin_delete_conf_cb = CallbackData("admin_delete_conf", "user_id")
