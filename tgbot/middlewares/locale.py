197b8ca209ec81c09cebebbfec2d06d884cd8d42
Ninnjah <yand.man4@gmail.com>
2022-06-19 04:28:43 +0300

    Update localisation middleware docstring

--- tgbot/middlewares/locale.py
+++ tgbot/middlewares/locale.py
@@ -1,9 +1,19 @@
 """This middleware need for bot localisation
-run `pybabel extract --input_dir=./tgbot -o locales/teamder-bot.pot` to
-extract all locale strings
 
-run `pybabel compile -d locales -D teamder-bot` to compile all
-localisation files
+Step 1: extract texts
+    run `pybabel extract --input_dir=./tgbot -o locales/teamder-bot.pot` to
+    extract all locale strings
+
+Step 2: create *.po files
+    `pybabel init -i locales/mybot.pot -d locales -D teamder-bot -l en`
+    `pybabel init -i locales/mybot.pot -d locales -D teamder-bot -l ru`
+
+Step 3: translate texts located in locales/{language}/LC_MESSAGES/mybot.po
+    To open .po file you can use basic text editor or any PO editor, e.g. https://poedit.net/
+
+Step 4: compile translations
+    run `pybabel compile -d locales -D teamder-bot` to compile all
+    localisation files
 """
 from pathlib import Path
 
