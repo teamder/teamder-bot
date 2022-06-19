4d00ee04139124c824b12231a31d57b5033c498a
Ninnjah <yand.man4@gmail.com>
2022-06-19 06:03:43 +0300

    Add list admins handler

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -60,6 +60,46 @@ async def list_users(m: Message, repo: Repo):
         await m.answer(_("No users was found"))
 
 
+async def list_admins(m: Message, repo: Repo):
+    # Get all admins from database
+    user_list = await repo.list_admins()
+
+    # If any admin was found
+    if user_list:
+        msg_text: str = ""
+
+        # Generate message text
+        for num, user in enumerate(user_list, start=1):
+            username = f"@{user.username}" if user.username is not None else ""
+            msg_text += _(
+                "{num}. {user_id} "
+                "<a href='tg://user?id={user_id}'><b>{fullname}</b></a> "
+                "{username}[{date}]\n"
+            ).format(
+                num=num,
+                user_id=user.user_id,
+                fullname=user.fullname,
+                username=username,
+                date=user.created_on
+            )
+
+        # If message long than maximum possible message
+        # then split message text on parts
+        if len(msg_text) > parts.MAX_MESSAGE_LENGTH:
+            for message in parts.safe_split_text(
+                msg_text, split_separator="\n"
+            ):
+                await m.answer(message)
+
+        # Else simply send this message text
+        else:
+            await m.answer(msg_text)
+
+    # If no admin was found then send message about it
+    else:
+        await m.answer(_("No admins was added"))
+
+
 def register_admin(dp: Dispatcher):
     dp.register_message_handler(
         admin_panel, commands=["admin"],
@@ -67,6 +107,11 @@ def register_admin(dp: Dispatcher):
     )
     dp.register_message_handler(
         list_users, lambda m: m.text == _("List users"),
+        state="*", role=UserRole.ADMIN
+    )
+
+    dp.register_message_handler(
+        list_users, lambda m: m.text == _("List admins"),
         state="*", role=UserRole.ADMIN
     )
     # # or you can pass multiple roles:
