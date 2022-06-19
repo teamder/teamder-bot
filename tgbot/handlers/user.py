76985d0ee5e9ef4f6ea41727b8d617eb51826dd2
Ninnjah <yand.man4@gmail.com>
2022-06-19 04:17:35 +0300

    Update user start handler

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,12 +1,23 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+from tgbot.handlers.inline import example_kb
+from tgbot.middlewares.locale import _
 from tgbot.services.repository import Repo
 
 
 async def user_start(m: Message, repo: Repo):
-    await repo.add_user(m.from_user.id)
-    await m.reply("Hello, user!")
+    await repo.add_user(
+        user_id=m.from_user.id,
+        firstname=m.from_user.first_name,
+        lastname=m.from_user.last_name,
+        fullname=m.from_user.full_name
+    )
+
+    await m.reply(
+        _("Hello, user!"),
+        reply_markup=example_kb.get_kb(m.from_user.id)
+    )
 
 
 def register_user(dp: Dispatcher):
