f308f3ae0cf7af0fe45322d1aa58ab29b951c53a
Ninnjah <yand.man4@gmail.com>
2022-06-19 04:06:36 +0300

    Update admin start handler

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -2,12 +2,21 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
+from tgbot.handlers.reply import admin_kb
+from tgbot.middlewares.locale import _
 from tgbot.models.role import UserRole
 from tgbot.services.repository import Repo
 
 
 async def admin_start(m: Message):
-    await m.reply("Hello, admin!")
+    await m.reply(
+        _(
+            "Hello {user_name}!\nYou in the admin panel"
+        ).format(
+            user_name=m.from_user.first_name
+        ),
+        reply_markup=admin_kb.get_kb()
+    )
 
 
 def register_admin(dp: Dispatcher):
