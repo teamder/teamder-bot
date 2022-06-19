ba26261fcd16d0eb39d69accbeb9dcc44b926093
Ninnjah <yand.man4@gmail.com>
2022-06-19 04:13:43 +0300

    Add inline keyboard example

---
+++ tgbot/handlers/inline/example_kb.py
@@ -0,0 +1,24 @@
+"""Example of inline keyboard. Callback data is neccessary"""
+from aiogram.types.inline_keyboard import InlineKeyboardButton, \
+    InlineKeyboardMarkup
+
+from tgbot.cb_data import example_cb
+from tgbot.middlewares.locale import _
+
+
+def get_kb(user_id: int) -> InlineKeyboardMarkup:
+    # Create keyboard
+    keyboard = InlineKeyboardMarkup(row_width=1)
+
+    # Add button with text and callback data
+    keyboard.add(
+        InlineKeyboardButton(
+            text=_("Ping!"),
+            callback_data=example_cb.new(
+                some_data=user_id
+            )
+        )
+    )
+
+    # Return keyboard
+    return keyboard
