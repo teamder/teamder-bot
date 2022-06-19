ff0f08bc9864f51db5bd3903179f63489a87d92a
Ninnjah <yand.man4@gmail.com>
2022-06-19 03:29:52 +0300

    Add some database query methods

--- tgbot/services/repository.py
+++ tgbot/services/repository.py
@@ -1,26 +1,104 @@
-from typing import List
+from typing import Dict, Optional
+
+from sqlalchemy import select, update
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio.engine import AsyncConnection
+
+from tgbot.database.tables import users
 
 
 class Repo:
     """Db abstraction layer"""
 
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self, conn: AsyncConnection):
+        self.conn: AsyncConnection = conn
 
     # users
-    async def add_user(self, user_id) -> None:
-        """Store user in DB, ignore duplicates"""
-        # await self.conn.execute(
-        #     "INSERT INTO tg_users(userid) VALUES $1 ON CONFLICT DO NOTHING",
-        #     user_id,
-        # )
+    async def add_user(
+        self,
+        user_id: int,
+        firstname: str,
+        lastname: str,
+        fullname: str,
+        lang: Optional[str] = None
+    ) -> None:
+        """Store user in DB, ignore duplicates
+
+        :param user_id: User's telegram id
+        :type user_id: int
+        :param firstname: User's firstname
+        :type firstname: str
+        :param lastname: User's lastname
+        :type lastname: str
+        :param lastname: User's fullname
+        :type lastname: str
+        :param lang: Language str in ISO 639-1 standart
+        :type lang: str
+        """
+        # Create insert statement
+        stmt = insert(users).values(
+            user_id=user_id,
+            firstname=firstname,
+            lastname=lastname,
+            fullname=fullname,
+            lang=lang
+        ).on_conflict_do_nothing()
+
+        # Execute statement
+        await self.conn.execute(stmt)
+        # Commit changes
+        await self.conn.commit()
         return
 
-    async def list_users(self) -> List[int]:
+    async def get_user(self, user_id: int) -> Optional[dict]:
+        """Returns user from database by user id
+
+        :param user_id: User telegram id
+        :type user_id: int
+        :return: User data from database or None if user not exists
+        :rtype: Optional[dict]
+        """
+        # Create statement
+        stmt = select(users).where(
+            users.c.user_id == user_id
+        )
+
+        # Execute statement
+        res = self.conn.execute(stmt)
+        try:
+            # Try to return one result
+            return res.mappings().one()
+
+        except NoResultFound:
+            # If no results found return None
+            return None
+
+    async def list_users(self) -> Optional[Dict[str]]:
         """List all bot users"""
-        return [
-            # row[0]
-            # async for row in self.conn.execute(
-            #     "select userid from tg_users",
-            # )
-        ]
+        # Create statement
+        stmt = select(users)
+
+        # Execute statement
+        res = await self.conn.execute(stmt)
+        # Return all found data in list of dicts or None
+        return res.mappings().all()
+
+    async def update_user_lang(self, user_id: int, lang: str) -> None:
+        """Updates user language
+
+        :param user_id: User's telegram id
+        :type user_id: int
+        :param lang: Language str in ISO 639-1 standart
+        :type lang: str
+        """
+        # Create statement
+        stmt = update(users).values(
+            lang=lang
+        ).where(
+            users.c.user_id == user_id
+        )
+
+        await self.conn.execute(stmt)
+        await self.conn.commit()
+        return
