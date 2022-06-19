36c4fadf24295dc04fa33a49a97e25d602bc98a3
Ninnjah <yand.man4@gmail.com>
2022-06-19 06:21:31 +0300

    Add admin add confirm inline keyboard

---
+++ tgbot/handlers/inline/admin/admin_add_conf_kb.py
@@ -0,0 +1,29 @@
+from aiogram.types.inline_keyboard import InlineKeyboardButton, \
+    InlineKeyboardMarkup
+
+from tgbot.cb_data import admin_add_conf_cb
+from tgbot.middlewares.locale import _
+
+
+def get_kb(user_id: int) -> InlineKeyboardMarkup:
+    # Create keyboard
+    keyboard = InlineKeyboardMarkup(row_width=2)
+
+    # Add button with text and callback data
+    keyboard.add(
+        InlineKeyboardButton(
+            text=_("Yes"),
+            callback_data=admin_add_conf_cb.new(
+                user_id=user_id
+            )
+        ),
+        InlineKeyboardButton(
+            text=_("No"),
+            callback_data=admin_add_conf_cb.new(
+                user_id=None
+            )
+        ),
+    )
+
+    # Return keyboard
+    return keyboard
