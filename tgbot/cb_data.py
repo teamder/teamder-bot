6897a58a90d7cdd394b5114230a8ba19f8993ee9
Ninnjah <yand.man4@gmail.com>
2022-06-19 06:34:12 +0300

    Add cancel callback data

--- tgbot/cb_data.py
+++ tgbot/cb_data.py
@@ -2,5 +2,6 @@ from aiogram.utils.callback_data import CallbackData
 
 # Callback data just for example
 example_cb = CallbackData("example", "some_data")
+cancel_cb = CallbackData("cancel")
 admin_add_conf_cb = CallbackData("admin_add_conf", "user_id")
 admin_delete_conf_cb = CallbackData("admin_delete_conf", "user_id")
